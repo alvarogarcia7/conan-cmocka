8c1d105d228319f93b9caa0ad4ea533da26fcce6
Samuel Marks <samuelmarks@gmail.com>
2018-10-02 23:55:45 +1000

    Removed dead code; updated homepage

--- conanfile.py
+++ conanfile.py
@@ -9,18 +9,16 @@ class CmockaConan(ConanFile):
     version = '1.1.3'
     sha256 = '43eabcf72a9c80e3d03f7c8a1c04e408c18d2db5121eb058a3ef732a9dfabfaf'
     license = 'Apache License 2.0'
-    homepage = 'https://github.com/elventear/cmocka'
+    homepage = 'https://cmocka.org'
     url = 'https://github.com/SamuelMarks/conan-cmocka'
     description = 'an elegant unit testing framework for C with support for mock objects. It only requires the standard C library, works on a range of computing platforms (including embedded) and with different compilers.'
     no_copy_source = True
     find_pkg = 'FindCMocka.cmake'
     exports_sources = ['include/*', 'FindCMocka.cmake']
-
     settings = 'os', 'compiler', 'build_type', 'arch'
     options = {'shared': [True, False]}
     default_options = 'shared=False'
     generators = 'cmake'
-
 
     def source(self):
         archive_name = '{name}-{version}.tar.xz'.format(name=self.name, version=self.version)
@@ -31,8 +29,6 @@ class CmockaConan(ConanFile):
         unzip(archive_name)
         remove(archive_name)
         rename('{name}-{version}'.format(name=self.name, version=self.version), self.name)
-        # download('https://raw.githubusercontent.com/owncloud/client/master/cmake/modules/{}'.format(self.find_pkg), self.find_pkg)
-        # check_sha256(self.find_pkg, 'e8c4faa50f51bb3aefb1a4714cf2abd26db8ff35e08939c74a92f0b0b4ddb4de')
 
     def build(self):
         cmake = CMake(self)
@@ -42,26 +38,10 @@ class CmockaConan(ConanFile):
     def package(self):
         self.copy(pattern=self.find_pkg, dst='.', src='.', keep_path=True)
         self.copy("*.h", dst="include", src=self.name, keep_path=True)
-        # self.copy("*hello.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
-
-    '''
-    def package(self):
-        self.copy(pattern=self.find_pkg, dst='.', src='.', keep_path=True)
-        # Copy the license files
-        self.copy(path.join(self.name, 'doc', 'that_style', 'LICENSE'), dst='licenses', src=self.name)
-        # Copying headers
-        self.copy(pattern='*.h', dst='include', src=path.join(self.name, 'include'), 
-keep_path=True)
-    '''
-
-    '''
-    def package_id(self):
-        self.info.header_only()
-    '''
 
     def package_info(self):
         self.cpp_info.libs = [self.name]
