66fcd8c489322bfca183e6b7bc6cce50bce9eb58
Samuel Marks <samuelmarks@gmail.com>
2018-10-02 13:39:11 +1000

    Non header-only; improve docs

--- README.md
+++ README.md
@@ -4,6 +4,42 @@ conan-cmocka
 Packaging up cmocka in conan.
 
 ## Build
+```bash
+conan create . <your_username>/<your_channel>
+```
+## Usage
 
-    conan create . <your_username>/<your_channel>
+Add this to your CMakeLists.txt:
+```cmake
+enable_testing()
 
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()
+
+include(CTest)
+
+find_package(cmocka REQUIRED)
+include_directories(${CONAN_INCLUDE_DIRS}/include)
+add_executable(test_0 test/test_0.c)
+```
+
+Where `test/test_0.c` contains:
+```c
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include "cmocka.h"
+
+/* A test case that does nothing and succeeds. */
+static void null_test_success(void **state) {
+    (void) state; /* unused */
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+            cmocka_unit_test(null_test_success),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
+```
--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,6 @@
 from os import rename, remove, path
 
-from conans import ConanFile
+from conans import ConanFile, CMake
 from conans.tools import download, check_sha256, unzip
 
 
@@ -16,9 +16,17 @@ class CmockaConan(ConanFile):
     find_pkg = 'FindCMocka.cmake'
     exports_sources = ['include/*', 'FindCMocka.cmake']
 
+    settings = 'os', 'compiler', 'build_type', 'arch'
+    options = {'shared': [True, False]}
+    default_options = 'shared=False'
+    generators = 'cmake'
+
+
     def source(self):
         archive_name = '{name}-{version}.tar.xz'.format(name=self.name, version=self.version)
-        download('https://cmocka.org/files/{ver}/{archive_name}'.format(ver=self.version.rpartition('.')[0], archive_name=archive_name), archive_name)
+        download('https://cmocka.org/files/{ver}/{archive_name}'.format(
+            ver=self.version.rpartition('.')[0], archive_name=archive_name
+        ), archive_name)
         check_sha256(archive_name, self.sha256)
         unzip(archive_name)
         remove(archive_name)
@@ -26,15 +34,35 @@ class CmockaConan(ConanFile):
         # download('https://raw.githubusercontent.com/owncloud/client/master/cmake/modules/{}'.format(self.find_pkg), self.find_pkg)
         # check_sha256(self.find_pkg, 'e8c4faa50f51bb3aefb1a4714cf2abd26db8ff35e08939c74a92f0b0b4ddb4de')
 
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=self.name)
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern=self.find_pkg, dst='.', src='.', keep_path=True)
+        self.copy("*.h", dst="include", src=self.name, keep_path=True)
+        # self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    '''
     def package(self):
         self.copy(pattern=self.find_pkg, dst='.', src='.', keep_path=True)
         # Copy the license files
         self.copy(path.join(self.name, 'doc', 'that_style', 'LICENSE'), dst='licenses', src=self.name)
         # Copying headers
-        self.copy(pattern='*.h', dst='include', src=path.join(self.name, 'include'), keep_path=True)
+        self.copy(pattern='*.h', dst='include', src=path.join(self.name, 'include'), 
+keep_path=True)
+    '''
 
+    '''
     def package_id(self):
         self.info.header_only()
+    '''
 
     def package_info(self):
+        self.cpp_info.libs = [self.name]
         self.env_info.CMAKE_MODULE_PATH = [self.package_folder]
